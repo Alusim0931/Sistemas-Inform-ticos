fcfdf3dc7da3ba6d60783c0283670ed38cdc9f2e
Rauh!! <115715375+Alusim0931@users.noreply.github.com>
2023-03-31 17:16:45 +0200

    Update uv.sh

--- uv.sh
+++ uv.sh
@@ -2,7 +2,7 @@ DIR_DESTINO="/mnt/nas/uv/$(date +"%-m")/$(date +"%-d")"
 
 wget -P "${DIR_DESTINO}" https://www.aemet.es/imagenes_d/eltiempo/prediccion/radiacionuv/uvi_PEN_24.gif
 
-wget -P "${DIR_DESTINO}" https://www.aemet.es/imagenes_d/eltiempo/prediccion/radiacionuv/uvi_PEN_48.gif
+wget -P "${DIR_DESTINO}" https://www.aemet.es/imagenes_d/eltiempo/prediccion/radiacionuv/uvi_PEN_48.gif 
 
 wget -P "${DIR_DESTINO}" https://www.aemet.es/imagenes_d/eltiempo/prediccion/radiacionuv/uvi_PEN_72.gif
 
