ad46bbf596bd7fad4f48123dc37b4297ddfb9a29
Rauh!! <115715375+Alusim0931@users.noreply.github.com>
2023-03-31 17:11:21 +0200

    Update nules.sh

--- nules.sh
+++ nules.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-DIR_DESTINO="/mnt/nas/webcams/Nules$(date +"%-m")/$(date +"%-d")"
+DIR_DESTINO="/mnt/nas/webcams/nules$(date +"%-m")/$(date +"%-d")"
 
 if [ ! -d "${DIR_DESTINO}" ]; then
     exit 1
 fi
 
-wget -P "${DIR_DESTINO}" "https://www.avametnuvol.es/estacions/c06m082e04/webcam/webcam.jpg?data=20230331124432"
-mv "${DIR_DESTINO}"/webcam.jpg "${DIR_DESTINO}"/"catilavella $(date +%H%M).jpg"
+wget -P "${DIR_DESTINO}" "https://www.avametnuvol.es/estacions/c06m082e04/webcam/webcam.jpg"
+mv "${DIR_DESTINO}"/webcam.jpg "${DIR_DESTINO}"/"nules $(date +%H%M).jpg"
